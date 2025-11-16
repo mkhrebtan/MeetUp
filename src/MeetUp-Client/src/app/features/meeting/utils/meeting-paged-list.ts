403a38846cba2d52a91917b9d6555484c0f40afd
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-11-17 00:42:36 +0200

    add meeting paged list component for managing participant pagination

---
+++ src/MeetUp-Client/src/app/features/meeting/utils/meeting-paged-list.ts
@@ -0,0 +1,20 @@
+﻿import {PagedList} from '../../../shared/utils/paged-list';
+import {Participant} from 'livekit-client';
+
+export class MeetingPagedList extends PagedList<Participant> {
+  constructor(items: Participant[], pageSize: number) {
+    super(items, pageSize);
+  }
+
+  public bringToFront(item: Participant): void {
+    const index = this.items().indexOf(item);
+    if (index !== -1) {
+      this._items.update(items => {
+        const newItems = [...items];
+        const [removed] = newItems.splice(index, 1);
+        newItems.unshift(removed);
+        return newItems;
+      });
+    }
+  }
+}
