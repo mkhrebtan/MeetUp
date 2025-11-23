f5fa5f378ce8b1462a25901b48c877e9d9a9179b
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-11-23 02:22:57 +0200

    feat: add records component for managing and viewing meeting recordings

---
+++ src/MeetUp-Client/src/app/features/records/records.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RecordsComponent } from './records.component';
+
+export const recordsRoutes: Routes = [
+  {
+    path: '',
+    component: RecordsComponent,
+  },
+];
