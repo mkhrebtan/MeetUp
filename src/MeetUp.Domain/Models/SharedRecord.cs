5499cef18cd06630e0e31bc121cb302b1f7c6fa2
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-12-07 20:48:39 +0200

    feat: Implement SharedRecord model and configuration with database migration

---
+++ src/MeetUp.Domain/Models/SharedRecord.cs
@@ -0,0 +1,16 @@
+﻿using MeetUp.Domain.Abstraction;
+
+namespace MeetUp.Domain.Models;
+
+public class SharedRecord : Model
+{
+    public Guid OwnerId { get; set; }
+    
+    public Guid RecipientId { get; set; }
+    
+    public required string StorageKey { get; set; }
+    
+    public required string FileName { get; set; }
+    
+    public DateTime RecordCreatedAt { get; set; }
+}
